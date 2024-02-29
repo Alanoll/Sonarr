9556dd6fbaf625ff08d5f3bdd075e61a02b69cbc
Mark McDowall <mark@mcdowall.ca>
2024-02-28 21:38:22 -0800

    New: Release Type (Single/Multi episode and Season Pack) for Custom Formats
    Closes #3562

---
+++ src/NzbDrone.Core/Parser/Model/ReleaseType.cs
@@ -0,0 +1,10 @@
+namespace NzbDrone.Core.Parser.Model
+{
+    public enum ReleaseType
+    {
+        Unknown = 0,
+        SingleEpisode = 1,
+        MultiEpisode = 2,
+        SeasonPack = 3
+    }
+}
